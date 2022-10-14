ed8d93c02d520c17a9f631eeaf98abdf2bf8cb82
rkqls8522 <68676639+rkqls8522@users.noreply.github.com>
2022-10-14 14:39:10 +0900

    review/create

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Review
+from .forms import ReviewForm
 
 # Create your views here.
+
+
+def create(request):
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            context = {
+                "form": form,
+            }
+            return render(request, "reviews/create.html", context)
+    else:
+        form = ReviewForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "reviews/create.html", context)
