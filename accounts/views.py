43b8022fdec282df122c9ad131743ae4b70ecad9
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-10-14 14:08:06 +0900

    accounts/password

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm,CustomUserChangeForm
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
 
 # Create your views here.
 
@@ -52,8 +53,7 @@ def detail(request, pk):
     return render(request, 'accounts/detail.html', context)
 
 @login_required
-def update(request, pk):
-
+def update(request):
     if request.method=="POST":
         form = CustomUserChangeForm(request.POST, instance=request.user)
         if form.is_valid():
@@ -65,6 +65,21 @@ def update(request, pk):
         'form':form,
     }
     return render(request,'accounts/update.html',context)
+
+@login_required
+def change_password(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('accounts:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        "form": form,
+    }
+    return render(request, 'accounts/password.html', context)
 
 def logout(request):
     auth_logout(request)
