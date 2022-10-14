968fd8755c224a36deeb26104cd767a265e81b14
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-10-14 16:18:52 +0900

    foreign_key

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm,CustomUserChangeForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
@@ -13,24 +13,23 @@ def index(request):
     users = get_user_model().objects.all()
 
     context = {
-        'users' : users,
+        "users": users,
     }
 
-    return render(request, 'accounts/index.html', context)
+    return render(request, "accounts/index.html", context)
+
 
 def signup(request):
     if request.method == "POST":
-        form=CustomUserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('accounts:login')
+            return redirect("accounts:login")
     else:
-        form=CustomUserCreationForm()
-    context = {
-        'form':form
-    }
+        form = CustomUserCreationForm()
+    context = {"form": form}
 
-    return render(request,'accounts/signup.html',context)
+    return render(request, "accounts/signup.html", context)
 
 
 def login(request):
@@ -38,34 +37,35 @@ def login(request):
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            return redirect(request.GET.get('next') or 'accounts:index')
+            return redirect(request.GET.get("next") or "accounts:index")
     else:
         form = AuthenticationForm()
-    context = {
-        'form':form
-    }
-    return render(request,'accounts/login.html',context)
+    context = {"form": form}
+    return render(request, "accounts/login.html", context)
+
 
 def detail(request, pk):
     user = get_user_model().objects.get(pk=pk)
     context = {
         "user": user,
     }
-    return render(request, 'accounts/detail.html', context)
+    return render(request, "accounts/detail.html", context)
+
 
 @login_required
 def update(request):
-    if request.method=="POST":
+    if request.method == "POST":
         form = CustomUserChangeForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect('accounts:detail',request.user.pk)
+            return redirect("accounts:detail", request.user.pk)
     else:
-        form=CustomUserChangeForm(instance=request.user)
-    context={
-        'form':form,
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        "form": form,
     }
-    return render(request,'accounts/update.html',context)
+    return render(request, "accounts/update.html", context)
+
 
 @login_required
 def change_password(request):
@@ -74,19 +74,21 @@ def change_password(request):
         if form.is_valid():
             form.save()
             update_session_auth_hash(request, form.user)
-            return redirect('accounts:index')
+            return redirect("accounts:index")
     else:
         form = PasswordChangeForm(request.user)
     context = {
         "form": form,
     }
-    return render(request, 'accounts/password.html', context)
+    return render(request, "accounts/password.html", context)
+
 
 def logout(request):
     auth_logout(request)
-    return redirect('accounts:index')
+    return redirect("reviews:index")
+
 
 def delete(request):
     request.user.delete()
     auth_logout(request)
-    return redirect('accounts:index')
+    return redirect("accounts:index")
--- reviews/models.py
+++ reviews/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
 
 class Review(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField()
-    movie_name=models.CharField(max_length=20)
-    grade=models.IntegerField()
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
+    movie_name = models.CharField(max_length=20)
+    grade = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
