a6ff3598ab5bb2ddb8ff01c79427b140870a6d31
rkqls8522 <68676639+rkqls8522@users.noreply.github.com>
2022-10-14 10:36:55 +0900

    accounts/signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .forms import CustomUserCreationForm
+
+
 
 # Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        form=CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form=CustomUserCreationForm()
+            context = {
+                'form':form
+            }
+            return render(request,'accounts/signup.html',context)
+    else:
+        form=CustomUserCreationForm()
+    context = {
+        'form':form
+    }
+
+    return render(request,'accounts/signup.html',context)
